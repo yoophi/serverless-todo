c297ea97ab7a98afa05a581f3acce183790f3d0f
Pyunghyuk Yoo <yoophi@gmail.com>
2021-09-18 21:55:47 +0900

    ✨ NEW: Todo List

---
+++ todo-api/todo_api/todos.py
@@ -0,0 +1,7 @@
+TODO_DATA = [
+    {"id": 1, "title": "study python", "completed": False, },
+    {"id": 2, "title": "study lambda", "completed": False, },
+    {"id": 3, "title": "write codes", "completed": False, },
+    {"id": 4, "title": "study aws sam", "completed": False, },
+    {"id": 5, "title": "deploy codes", "completed": False, },
+]
